70d9687c8066238d980ad5c66cab7cefade63f8b
ao307 <56132081+ao307@users.noreply.github.com>
2021-03-25 13:24:37 +0200

    Operations by switch

---
+++ bin/Operation_switch.dart
@@ -0,0 +1,5 @@
+import 'package:Operation_switch/Operation_switch.dart' as Operation_switch;
+
+void main(List<String> arguments) {
+  print('Hello world: ${Operation_switch.calculate()}!');
+}
---
+++ test/Operation_switch_test.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+void main() {
+  double num1 = 1, num2 = 0, sum; //هعدل على الارقام اللى انا عايز احسبها
+  //String operation = stdin.readLineSync();
+  String operation = "9"; //هحدد العملية اللى عايزها من هنا
+
+  switch (operation) {
+    case "+":
+      sum = num1 + num2;
+      print("Sum= $sum");
+      break;
+    case "-":
+      sum = num1 - num2;
+      print("Sum= $sum");
+      break;
+    case "*":
+      sum = num1 * num2;
+      print("Sum= $sum");
+      break;
+    case "/":
+      if (num2 ==
+          0) //بعرف منها لو الرقم التانى صفر يظهر رسالة ان مفيش قسمة على صفر
+      {
+        print("Can't divide by zero");
+      } else {
+        sum = num1 / num2;
+        print("Sum= $sum");
+      }
+      break;
+
+    default:
+      print(
+          'Enter correct operation "+,-,*,/"'); //عشان لو دخلت اي حاجه غير(+,-,*,/)
+
+  }
+}
